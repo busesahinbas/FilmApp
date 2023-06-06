b2940e861b1fe55c7cb97b10f786fc3246325200
busesahinbas <85345494+busesahinbas@users.noreply.github.com>
2023-06-06 15:50:06 +0300

    [chore] first commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by Buse Şahinbaş on 6.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
